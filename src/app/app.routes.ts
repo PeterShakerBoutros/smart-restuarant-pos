fec5a770321a017a14f5bf08745c275889f4f454
Pato0t <petershaker16@gmail.com>
2026-07-13 16:59:57 +0300

    create dashboard layout with header and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+
+import { DashboardLayoutComponent } from './layout/dashboard-layout/dashboard-layout.component';
 import { OrdersPageComponent } from './features/orders/pages/orders-page/orders-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: OrdersPageComponent
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: OrdersPageComponent
+      }
+    ]
   }
 ];
